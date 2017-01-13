7b2fa6047da7fca773c2b064951cd8efc0705147
Maroxis <wola1997@interia.pl>
2017-01-13 11:37:07 +0100

    typo

--- protoRebuild.js
+++ protoRebuild.js
@@ -2,7 +2,7 @@ function rebuild(population){
   reattachMethods(population,Population)
   reattachMethods(population.his,hist)
   reattachMethods(population.longHis,hist)
-  for (var i = 0; i < population.posize; i++){
+  for (var i = 0; i < population.popsize; i++){
     reattachMethods(population.rockets[i],Rocket)
     reattachMethods(population.rockets[i].dna,DNA)
   }
