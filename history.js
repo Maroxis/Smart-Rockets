be2f4368a3a5887447f605fe4a2c7c06a9a05b85
Maroxis <wola1997@interia.pl>
2017-01-21 15:25:48 +0100

    cleaning

--- history.js
+++ history.js
@@ -4,11 +4,7 @@ hist = function(size){
 }
 
 hist.prototype.log = function(score,generation){
-  if (this.el.length < this.size)
-     this.el.push([score,generation])
-   else{
-    for (var i = 0; i < this.size-1; i++)
-      this.el[i] = this.el[i+1]
-    this.el[this.size-1] = [score,generation]
-  }
+  if (this.el.length >= this.size)
+    this.el.slice(0,1)
+  this.el.push([score,generation])
 }
